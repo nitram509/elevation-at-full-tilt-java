cd716444bd35baf97401dbab443b3ee50353bb6c
nitram509 <maki@bitkings.de>
2015-09-19 17:52:41 +0200

    archiver with max filesize usage

--- src/main/java/de/bitkings/nitram509/Archiver.java
+++ src/main/java/de/bitkings/nitram509/Archiver.java
@@ -10,23 +10,38 @@ import java.io.IOException;
 
 public class Archiver implements Closeable {
 
+  private static final long MAX = 100_000_000;
   private long estimatedFileSized = 0;
-  private final TarArchiveOutputStream taos;
+  private int indexCounter = 0;
+  private TarArchiveOutputStream taos;
+  private String baseName;
 
-  public Archiver(String fileName) throws FileNotFoundException {
-    if (!fileName.endsWith(".tar")) {
-      fileName = fileName + ".tar";
-    }
-    taos = new TarArchiveOutputStream(new FileOutputStream(fileName));
+  public Archiver(String baseName) throws FileNotFoundException {
+    this.baseName = baseName;
+    createArchive();
+  }
+
+  private void createArchive() throws FileNotFoundException {
+    String name = String.format("%s.%04d.tar", baseName, indexCounter);
+    taos = new TarArchiveOutputStream(new FileOutputStream(name));
   }
 
   public void add(String name, byte[] data) throws IOException {
+    splitArchiveIfRequired(data.length);
     TarArchiveEntry entry = new TarArchiveEntry(name);
     entry.setSize(data.length);
     entry.setMode(600);
     taos.putArchiveEntry(entry);
     taos.write(data);
     taos.closeArchiveEntry();
+  }
+
+  private void splitArchiveIfRequired(int length) throws IOException {
+    if (estimatedFileSized + length > MAX) {
+      taos.close();
+      indexCounter++;
+      createArchive();
+    }
   }
 
   public void close() throws IOException {
