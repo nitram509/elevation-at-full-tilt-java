49c61a74a1df02266b3f14e3eef15e52d6585c42
nitram509 <maki@bitkings.de>
2015-09-27 00:11:35 +0200

    reworked learning test into reader

---
+++ src/main/java/de/bitkings/nitram509/srtm/metafile/GranuleMetaDataFileReader.java
@@ -0,0 +1,31 @@
+package de.bitkings.nitram509.srtm.metafile;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.sax.SAXSource;
+import java.io.InputStream;
+
+public class GranuleMetaDataFileReader {
+
+  public GranuleMetaDataFile read(InputStream xml) throws JAXBException, ParserConfigurationException, SAXException {
+    JAXBContext jc = JAXBContext.newInstance(GranuleMetaDataFile.class);
+    Unmarshaller unmarshaller = jc.createUnmarshaller();
+    GranuleMetaDataFile granuleMetaData = (GranuleMetaDataFile) unmarshaller.unmarshal(createNonValidatingXmlReader(xml));
+    return granuleMetaData;
+  }
+
+  private SAXSource createNonValidatingXmlReader(InputStream xml) throws ParserConfigurationException, SAXException {
+    SAXParserFactory spf = SAXParserFactory.newInstance();
+    spf.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+    spf.setFeature("http://xml.org/sax/features/validation", false);
+    XMLReader xmlReader = spf.newSAXParser().getXMLReader();
+    return new SAXSource(xmlReader, new InputSource(xml));
+  }
+}
---
+++ src/test/java/de/bitkings/nitram509/srtm/metafile/GranuleMetaDataFileReaderTest.java
@@ -0,0 +1,33 @@
+package de.bitkings.nitram509.srtm.metafile;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.StrictAssertions.assertThat;
+
+public class GranuleMetaDataFileReaderTest {
+
+  private static final String EXAMPLE_HGT_XML_RESOURCE_PATH = "/N00E006.hgt.xml";
+
+  private GranuleMetaDataFileReader reader;
+
+  @BeforeMethod
+  public void setUp() throws Exception {
+    reader = new GranuleMetaDataFileReader();
+  }
+
+  @Test
+  public void bounding_rectangle_can_be_read_via_JAXB_binding() throws Exception {
+    GranuleMetaDataFile granuleMetaData = reader.read(getClass().getResourceAsStream(EXAMPLE_HGT_XML_RESOURCE_PATH));
+
+    assertThat(granuleMetaData).isNotNull();
+    GranuleMetaDataFile.GranuleURMetaData.SpatialDomainContainer.HorizontalSpatialDomainContainer.BoundingRectangle boundingRectangle = granuleMetaData.getGranuleURMetaData().getSpatialDomainContainer().getHorizontalSpatialDomainContainer().getBoundingRectangle();
+    assertThat(boundingRectangle.getWestBoundingCoordinate()).isEqualTo(new BigDecimal("5.99916667"));
+    assertThat(boundingRectangle.getNorthBoundingCoordinate()).isEqualTo(new BigDecimal("1.00083333"));
+    assertThat(boundingRectangle.getEastBoundingCoordinate()).isEqualTo(new BigDecimal("7.00083333"));
+    assertThat(boundingRectangle.getSouthBoundingCoordinate()).isEqualTo(new BigDecimal("-0.00083333"));
+  }
+
+}
