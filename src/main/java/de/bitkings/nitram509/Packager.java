4166de06e9ea1d674089c521450102c9927deb05
nitram509 <maki@bitkings.de>
2015-09-26 18:21:44 +0200

    analyse buckets for ranges for further optimization

--- src/main/java/de/bitkings/nitram509/Packager.java
+++ src/main/java/de/bitkings/nitram509/Packager.java
@@ -1,12 +1,13 @@
 package de.bitkings.nitram509;
 
-import com.sun.tools.internal.ws.processor.util.DirectoryUtil;
 import net.jpountz.lz4.LZ4Compressor;
 import net.jpountz.lz4.LZ4Factory;
-import org.apache.commons.io.DirectoryWalker;
 import org.apache.commons.io.IOUtils;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
@@ -29,6 +30,33 @@ public class Packager {
     File[] zipFiles = new File(basePath).listFiles(path -> {
       return path.getName().endsWith(".hgt.zip");
     });
+    //compressFiles(zipFiles);
+    determineDelta(zipFiles);
+  }
+
+  private void determineDelta(File[] zipFiles) throws Exception {
+    for (File zipFile : zipFiles) {
+      Lz4Bucket lz4Bucket = openFromZipFile(zipFile);
+      analyze(lz4Bucket);
+    }
+  }
+
+  private void analyze(Lz4Bucket lz4Bucket) {
+    int min = 0;
+    int max = 0;
+    ByteBuffer hgt = ByteBuffer.wrap(lz4Bucket.data);
+    for (int i = 0; i < (lz4Bucket.data.length - 1) / 2; i++) {
+      int height1 = hgt.get(i * 2) & 0xff;
+      int height2 = hgt.get(i * 2 + 1) & 0xff;
+      int height = height1 << 8 | height2;
+      min = Math.min(min, height);
+      max = Math.max(max, height);
+    }
+    if (max - min < 256)
+      System.out.println(String.format("min=%d,max=%d,delta=%d", min, max, max - min));
+  }
+
+  private void compressFiles(File[] zipFiles) throws Exception {
     try (Archiver archiver = new Archiver("test")) {
       for (File zipFile : zipFiles) {
         Lz4Bucket lz4Bucket = openFromZipFile(zipFile);
