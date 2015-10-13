d748c976da09172053ff80189e88b8af87529e40
nitram509 <maki@bitkings.de>
2015-10-13 23:32:16 +0200

    prepared pre-process task

--- src/main/java/de/bitkings/nitram509/elevation/preprocess/PrepareSrtmDataTask.java
+++ src/main/java/de/bitkings/nitram509/elevation/preprocess/PrepareSrtmDataTask.java
@@ -1,4 +1,63 @@
 package de.bitkings.nitram509.elevation.preprocess;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ning.http.client.AsyncHttpClient;
+import com.ning.http.client.Response;
+import org.apache.commons.cli.*;
+import ratpack.config.ConfigData;
+
+import java.io.File;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
 public class PrepareSrtmDataTask {
+
+  private TaskConfig taskConfig;
+
+  public static void main(String[] args) {
+    try {
+      PrepareSrtmDataTask task = new PrepareSrtmDataTask();
+      task.readConfiguration();
+      task.download();
+      task.runTask(args);
+    } catch (ParseException exp) {
+      System.err.println("Parsing failed.  Reason: " + exp.getMessage());
+      System.exit(1);
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.exit(-1);
+    }
+  }
+
+  private void download() throws ExecutionException, InterruptedException {
+    AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+    Future<Response> f = asyncHttpClient.prepareGet("http://www.ning.com/").execute();
+    Response r = f.get();
+    System.out.println(r);
+  }
+
+  private void readConfiguration() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.registerSubtypes(TaskConfig.class);
+    ConfigData configData = ConfigData.builder(objectMapper)
+        .yaml(new File("application.yaml").toPath())
+        .env()
+        .sysProps()
+        .build();
+    taskConfig = configData.get(TaskConfig.class);
+  }
+
+  private void runTask(String[] args) throws Exception {
+    Options options = new Options();
+    options.addOption("t", false, "display current time");
+    CommandLineParser parser = new DefaultParser();
+    CommandLine cmd = parser.parse(options, args);
+    if (cmd.hasOption("t")) {
+      System.out.println("test");
+    } else {
+      // automatically generate the help statement
+      HelpFormatter formatter = new HelpFormatter();
+      formatter.printHelp(PrepareSrtmDataTask.class.getName(), options);
+    }
+  }
 }
---
+++ src/main/java/de/bitkings/nitram509/elevation/preprocess/TaskConfig.java
@@ -0,0 +1,16 @@
+package de.bitkings.nitram509.elevation.preprocess;
+
+import java.util.List;
+
+public class TaskConfig {
+
+  private String[] srtm3arcsecBaseUrls = new String[0];
+
+  public String[] getSrtm3arcsecBaseUrls() {
+    return srtm3arcsecBaseUrls;
+  }
+
+  public void setSrtm3arcsecBaseUrls(String[] srtm3arcsecBaseUrls) {
+    this.srtm3arcsecBaseUrls = srtm3arcsecBaseUrls;
+  }
+}
